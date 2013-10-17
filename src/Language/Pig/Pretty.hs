b694e6701ec6abd09a15afa8189db520a66461ea
Elise Huard <elise@jabberwocky.eu>
2013-10-17 09:42:16 +0200

    small reordering to make sense of structure

--- src/Language/Pig/Pretty.hs
+++ src/Language/Pig/Pretty.hs
@@ -9,20 +9,30 @@ import Text.ParserCombinators.Parsec
 import Data.List (intercalate)
 import Data.Tree hiding (Tree)
 
-toDataTree (PigString s) = Node ("string: " ++ s) []
+-- patterns:
+--  node name + list nodes
+--  node name + literal to display = terminalnode
+--  node name + arguments used as lists to use as nodes
+
+-- top level node
+toDataTree (PigSeq list) = Node "statement sequence" (map toDataTree list)
+
+-- non terminal nodes
 toDataTree (PigAssignment a b) = Node "assignment" (map toDataTree [a, b])
-toDataTree (PigIdentifier s) = Node ("identifier: " ++ s) []
-toDataTree (PigFilename s) = Node ("filename: \"" ++ s ++ "\"") []
 toDataTree (PigLoadClause a b c) = Node "load stmt" (map toDataTree [a, b, c])
-toDataTree (PigFunc a b) = Node ("function: " ++ a ++ "(" ++ prettyText b ++ ")") []
 toDataTree (PigSchema list) = Node "schema" (map toDataTree list)
-toDataTree (PigField (PigFieldName a) (PigFieldType b)) = Node ("field " ++ a ++ ": " ++ show b) []
 toDataTree (PigForeachClause a b) = Node "foreach stmt" (map toDataTree [a, b])
 toDataTree (PigTransforms list) = Node "transforms" (map toDataTree list)
-toDataTree (PigFlatten a b) = Node ("flatten " ++ a) (listToTree b)
-toDataTree (PigFieldName a) = Node ("field: " ++ a) []
+toDataTree (PigFlatten a b) = Node ("flatten " ++ a) [toDataTree b]
+toDataTree (PigTuple list) = Node ("tuple") (map toDataTree list)
 
-listToTree (PigTuple list) = map toDataTree list
+-- terminal nodes
+toDataTree (PigString s) = Node ("string: " ++ s) []
+toDataTree (PigIdentifier s) = Node ("identifier: " ++ s) []
+toDataTree (PigFilename s) = Node ("filename: \"" ++ s ++ "\"") []
+toDataTree (PigFunc a b) = Node ("function: " ++ a ++ "(" ++ prettyText b ++ ")") []
+toDataTree (PigField (PigFieldName a) (PigFieldType b)) = Node ("field " ++ a ++ ": " ++ show b) []
+toDataTree (PigFieldName a) = Node ("field: " ++ a) []
 
 prettyText :: PigNode -> String
 prettyText (PigArguments b) = intercalate "," $ map prettyText b
