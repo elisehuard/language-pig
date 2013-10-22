27302db9e5f04b88579ff42f5dfff0f256544b09
Elise Huard <elise@jabberwocky.eu>
2013-10-22 14:27:01 +0200

    no liftM left in code

--- src/Language/Pig/Parser/Parser.hs
+++ src/Language/Pig/Parser/Parser.hs
@@ -5,7 +5,6 @@ module Language.Pig.Parser.Parser (
 ) where
 
 import System.IO
-import Control.Monad
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
